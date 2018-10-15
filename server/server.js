acbbe0e5249e05eb064e44990e62c88fe884c234
Yohan Kim <yohan@yohanpro.com>
2018-10-15 19:37:14 +0900

    새로 만들기

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname,'../public');
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port,()=>{
+    console.log(`Server is up on port ${port}`);
+})
