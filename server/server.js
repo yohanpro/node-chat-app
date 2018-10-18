7d8791d68ce903b77681fa1a332b7379e2ed8a45
Yohan Kim <yohan@yohanpro.com>
2018-10-18 19:29:14 +0900

    emit message on Create Message

--- server/server.js
+++ server/server.js
@@ -14,13 +14,14 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
-    socket.emit('newMessage', {
-        from: 'yohanpro@yohanpro.com',
-        text: "See you then",
-        createAt: 123
-    });
+  
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit('newMessage',{
+            from:message.from,
+            text:message.text,
+            createAt:new Date().getTime()
+        })
     });
     socket.on('disconnect', () => {
         console.log('Client Disconnected');
