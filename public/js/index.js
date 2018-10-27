0f19a10e5f88362624098b5d46f4b16e2dccd727
Yohan Kim <yohan@yohanpro.com>
2018-10-27 13:18:47 +0900

    jquery 추가

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        // from: 'Andrew',
-        // text: 'Yup, that works for me.'
-    });
 });
 
 socket.on('disconnect', function () {
@@ -15,4 +10,17 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     console.log('newMessage', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}:${message.text}`);
+    jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function () {
+
+    });
 });
