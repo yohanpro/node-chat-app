86bda5a6ebb193374219742640667a574ec2311e
Yohan Kim <yohan@yohanpro.com>
2018-10-27 13:24:06 +0900

    jquery 오류

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,28 @@
 var socket = io();
 
 socket.on('connect', function () {
-    console.log('Connected to server');
+  console.log('Connected to server');
 });
 
 socket.on('disconnect', function () {
-    console.log('Disconnected from server');
+  console.log('Disconnected from server');
 });
 
 socket.on('newMessage', function (message) {
-    console.log('newMessage', message);
-    var li = jQuery('<li></li>');
-    li.text(`${message.from}:${message.text}`);
-    jQuery('#messages').append(li);
+  console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function (e) {
-    e.preventDefault();
-    socket.emit('createMessage', {
-        from: 'User',
-        text: jQuery('[name=message]').val()
-    }, function () {
+  e.preventDefault();
 
-    });
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  });
 });
